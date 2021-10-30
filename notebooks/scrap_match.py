f6372dfb2b80c4ce312689c8745c3871888788a7
zulrich91 <ulrichmobi2@gmail.com>
2021-10-30 22:20:04 +0200

    Feat:Handle more exceptions and print more messages to the logs

--- notebooks/scrap_match.py
+++ notebooks/scrap_match.py
@@ -2,6 +2,7 @@ from utils import *
 import pandas as pd
 import json
 import ast
+import time 
 
 
 fields = [  "dayofweek", "comp","round","venue","result", "squad", "opponent","game_started", 
@@ -22,27 +23,47 @@ columns = [ 'player', 'date', 'date_url', 'dayofweek', 'comp_url', 'comp',
             'match_report'
            ]
 result_df = pd.DataFrame(columns=columns)
+problematic_logs = []
+current_log = ''
+counter = 0
+START_TIME = time.time()
 
 def scrap_match_urls(id, name, logs):
-    # # logs = dict(json.loads(logs))
-    # logs = ast.literal_eval(json.dumps(logs))
-    print(logs)
     global result_df
-    players_name= unidecodePlayersName(name)
-    log_name = joined_player_name(players_name) + "-Match-Logs"
+    global current_log
+    global problematic_logs
+    global counter 
+    try: 
+        current_log = str(id)+"/"+str(logs)
+        players_name= unidecodePlayersName(name)
+        log_name = joined_player_name(players_name) + "-Match-Logs"
 
-    for log in logs.values():
-        matchLogUrl = BASE_PLAYER_URL + id + "/matchlogs/" + str(log) + "/summary/" + log_name
-        print("Scrapping ->: {}".format(matchLogUrl))
-        tables, player = get_tables(matchLogUrl)
-        scrap_result_df = get_frame(fields,tables[0], player)
-        if len(result_df) == 0 :
-            result_df = scrap_result_df.copy(deep=True)
-        else:
-            result_df = pd.concat([result_df, scrap_result_df],axis=0)
-    result_df.to_csv("final_scrapped_match_logs.csv", index=False)
-    # return result_df
+        for log in logs.values():
+            matchLogUrl = BASE_PLAYER_URL + str(id) + "/matchlogs/" + str(log) + "/summary/" + log_name
+            current_log = matchLogUrl
+            print("Scrapping ->: {} ".format(matchLogUrl))
+            tables, player = get_tables(matchLogUrl)
+            scrap_result_df = get_frame(fields,tables[0], player)
+            if len(result_df) == 0 :
+                result_df = scrap_result_df.copy(deep=True)
+            else:
+                result_df = pd.concat([result_df, scrap_result_df],axis=0)
+            counter=counter+1
+            print("Completed {} \nTotal Execution time {} seconds\n".format(counter, time.time()-START_TIME))
+        result_df.to_csv("../scrap_of_final_sample_matchlogs.csv", index=False)
+
+    except Exception as e:
+        print("Exception ->: {}\nConcering player with log(s) ->: {} ".format(e, str(current_log)))
+        problematic_logs.append(current_log)
+        counter=counter+1
+        print("Completed {} \nTotal Execution time {} seconds\n".format(counter, time.time()-START_TIME))
 
 if __name__ == "__main__":
-    for chunk in pd.read_csv("sample_logs_to_scrap.csv", chunksize=10):
-        chunk.apply(lambda x: scrap_match_urls(x["id"], x['name'], eval(x['logs'])), axis=1)
+    try:
+        for chunk in pd.read_csv("../data/final_sample_matchlogs.csv", chunksize=10):
+            chunk.apply(lambda x: scrap_match_urls(x["id"], x['name'], eval(x['logs'])), axis=1)
+    except Exception as e:
+        print("Exception ->: {}\nConcering player with logs ->: {} ".format(e, str(current_log)))
+        problematic_logs.append(current_log)
+    print("Completed Successfully!!!")
+    pd.DataFrame({'url':problematic_logs}).to_csv("probematic_logs.csv", index=False)
