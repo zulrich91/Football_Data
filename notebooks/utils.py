aa1ded5a5e674f8ef880761bd8b83b60ce6f927d
zulrich91 <ulrichmobi2@gmail.com>
2021-11-07 21:31:59 +0100

    Update:Funtions to scrap match metadata

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -11,6 +11,105 @@ HOME_PAGE = "https://fbref.com/"
 BASE_MATCH_LOG_URL = "https://fbref.com/en/players/19cda00b/matchlogs/2021-2022/summary/Angel-Di-Maria-Match-Logs"
 BASE_PLAYER_URL = "https://fbref.com/en/players/"
 
+def get_match_metadata(url):
+    url = HOME_PAGE+url
+    res = requests.get(url)
+    print("Scrapping ->: ",url)
+    ## The next two lines get around the issue with comments breaking the parsing.
+    comm = re.compile("<!--|-->")
+    # soup = BeautifulSoup(comm.sub("",res.text),'lxml')
+    soup = BeautifulSoup(comm.sub("",res.text),"html.parser")
+    inner_div = soup.find("div", class_="scorebox")
+    return inner_div
+
+def get_venue_attendance_referees(url):
+    try:
+        result_div = get_match_metadata(url)
+    except Exception as e:
+        return "","","","","","",""
+
+    try:
+        divs = result_div.find('div', class_="scorebox_meta")
+    except Exception as e:
+        return "","","","","","",""
+    
+    try:
+        venue = divs.find_all("small")[3].text
+    except Exception as e:
+        venue = '' 
+
+
+    try:
+        attendance = divs.find_all("small")[1].text
+    except Exception as e:
+        attendance = ""
+
+    try:
+        referee = unidecodePlayersName(divs.find_all("small")[5].find_all("span")[0].text).split(" (")[0]
+    except Exception as e:
+        referee=''
+        
+    try:
+        AR1 = unidecodePlayersName(divs.find_all("small")[5].find_all("span")[1].text).split(" (")[0]
+    except Exception as e:
+        AR1 =''
+    try:
+
+        AR2 = unidecodePlayersName(divs.find_all("small")[5].find_all("span")[2].text).split(" (")[0]
+    except Exception as e:
+        AR2=''
+    try:
+        fourth = unidecodePlayersName(divs.find_all("small")[5].find_all("span")[3].text).split(" (")[0]
+    except Exception as e:
+        fourth=''
+    try:
+        venue_time = divs.find("span", {"class":"venuetime"}).text.split(" (")[0]
+    except Exception as e:
+        venue_time=''
+    return [venue,attendance,referee, AR1, AR2,fourth,venue_time]
+
+def get_managers_and_captains(url):
+    
+    try:
+        result_div = get_match_metadata(url)
+    except Exception as e:
+
+        return "","","","","",""
+    try:
+        divs = result_div.find_all('div', class_='datapoint')
+    except Exception as e:
+
+        return "","","","","",""
+    try:
+        manager = unidecodePlayersName (divs[0].get_text().split(": ")[1].replace("</div>",''))
+    except Exception as e:
+        manager =''
+
+    try:
+        captain = unidecodePlayersName( divs[1].find("a", href=True).text)
+    except Exception as e:
+        captain =''
+
+    try:
+        captain_url = divs[1].find("a", href=True).attrs["href"]
+    except Exception as e: 
+        captain_url =''
+    try:
+        opponent_manager = unidecodePlayersName (divs[2].get_text().split(": ")[1].replace("</div>",''))
+    except Exception as e: 
+        opponent_manager=''
+    try:
+        opponent_captain = unidecodePlayersName (divs[3].find("a", href=True).text)
+    except Exception as e:
+        opponent_captain =''
+    try:
+
+        opponent_captain_url = divs[3].find("a", href=True).attrs["href"]
+    except Exception as e:
+        opponent_captain_url = ''
+    
+    return [manager,captain,captain_url, opponent_manager, opponent_captain, opponent_captain_url]
+
 def unidecodePlayersName(name):
     players_name = unidecode.unidecode(name)
     return players_name
